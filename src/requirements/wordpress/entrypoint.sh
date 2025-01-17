87eb2f3cca639caf707e309b595b9d681aa481cd
Ahmed Ghounami <aghounam@e2r2p14.1337.ma>
2025-01-17 09:41:35 +0100

    42inception is loading

--- src/requirements/wordpress/entrypoint.sh
+++ src/requirements/wordpress/entrypoint.sh
@@ -7,7 +7,10 @@ sed -i "s/username_here/$WORDPRESS_DB_USER/" /var/www/html/wp-config.php
 sed -i "s/password_here/$WORDPRESS_DB_PASSWORD/" /var/www/html/wp-config.php
 sed -i "s/localhost/$WORDPRESS_DB_HOST/" /var/www/html/wp-config.php
 
-
+echo $WORDPRESS_DB_NAME
+echo $WORDPRESS_DB_USER
+echo $WORDPRESS_DB_PASSWORD
+echo $WORDPRESS_DB_HOST
 
 # Start PHP-FPM and NGINX
 service php7.4-fpm start
